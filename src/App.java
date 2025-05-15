d03b8febca39ca1ec2e30fe7a50a9c859578b09f
dkhuy6128 <23017163@st.phenikaa-uni.edu.vn>
2025-05-15 08:13:03 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        User user = new User (name: "dkhuy", userID: "2005");
+        System.out.println(user);
     }
 }
